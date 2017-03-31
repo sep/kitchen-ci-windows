220398762acdd556f308c3b761bb52b08f015fa8
rmnowadly <rmnowadly@sep.com>
2017-03-31 09:52:43 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# kitchen-ci-windows
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: kitchen-ci-windows
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
