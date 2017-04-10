900c6d4b5437473aa78798f44d63c81aebb3e53f
rmnowadly <rmnowadly@sep.com>
2017-04-10 09:30:48 -0400

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rmnowadly@sep.com'
 license 'Apache license 2.0'
 description 'Installs/Configures kitchen-ci-windows'
 long_description 'Installs/Configures kitchen-ci-windows'
-version '0.8.0'
+version '0.8.1'
 
 supports 'windows', '2016'
 
